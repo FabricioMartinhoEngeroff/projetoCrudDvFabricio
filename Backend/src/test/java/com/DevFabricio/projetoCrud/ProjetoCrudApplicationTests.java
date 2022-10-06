6e58ed9c0adb70fa334854087995779d4d75e737
Fabricio Martinho Engeroff <fa.engeroff1996@gmail.com>
2022-10-06 07:28:48 -0300

    Backend projectCrud created

---
+++ Backend/src/test/java/com/DevFabricio/projetoCrud/ProjetoCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DevFabricio.projetoCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
