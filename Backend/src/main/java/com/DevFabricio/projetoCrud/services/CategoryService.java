b3baabdf740dd7f61bbedb0530b1cc1f4b2575d1
Fabricio Martinho Engeroff <fa.engeroff1996@gmail.com>
2022-10-10 08:31:53 -0300

    Find Category by id, exception handling

--- Backend/src/main/java/com/DevFabricio/projetoCrud/services/CategoryService.java
+++ Backend/src/main/java/com/DevFabricio/projetoCrud/services/CategoryService.java
@@ -1,12 +1,19 @@
 package com.DevFabricio.projetoCrud.services;
 
+
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.DevFabricio.projetoCrud.dto.CategoryDTO;
 import com.DevFabricio.projetoCrud.entities.Category;
 import com.DevFabricio.projetoCrud.repositories.CategoryRepository;
+import com.DevFabricio.projetoCrud.services.esceptions.EntityNotFoundException;
+
 
 @Service
 public class CategoryService {
@@ -14,7 +21,16 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
-	public List<Category> findAll(){
-		return repository.findAll();
+	@Transactional(readOnly = true)
+	public List<CategoryDTO> findAll(){
+		List<Category>list =repository.findAll();
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj =repository.findById(id);
+		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		return new CategoryDTO(entity);
 	}
 }
