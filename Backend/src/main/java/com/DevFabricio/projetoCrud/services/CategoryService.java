0689a6383ea8cc2f5c0e19dfc6ccee29fcaeec35
Fabricio Martinho Engeroff <fa.engeroff1996@gmail.com>
2022-10-11 08:36:01 -0300

    Category Delete

--- Backend/src/main/java/com/DevFabricio/projetoCrud/services/CategoryService.java
+++ Backend/src/main/java/com/DevFabricio/projetoCrud/services/CategoryService.java
@@ -6,12 +6,15 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.DevFabricio.projetoCrud.dto.CategoryDTO;
 import com.DevFabricio.projetoCrud.entities.Category;
 import com.DevFabricio.projetoCrud.repositories.CategoryRepository;
+import com.DevFabricio.projetoCrud.services.esceptions.DatabaseException;
 import com.DevFabricio.projetoCrud.services.esceptions.ResourceNotFoundException;
 
 import javax.persistence.EntityNotFoundException;
@@ -54,7 +57,19 @@ public class CategoryService {
 			}
 			catch(EntityNotFoundException e) {
 				throw new ResourceNotFoundException("Id not found " + id);
-	
-	}
-	}
+			}
+		}
+		
+		public void delete(Long id) {
+			try {
+			repository.deleteById(id);
+			}
+			catch(EmptyResultDataAccessException e) {
+				throw new ResourceNotFoundException("Id not found " + id);
+			}
+			catch (DataIntegrityViolationException e) {
+				throw new DatabaseException("Integrity violation");
+			}
+			
+		}
 }
