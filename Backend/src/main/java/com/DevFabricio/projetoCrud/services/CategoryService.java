90717d846aa4ee5205b61f480149d7c93c0743b8
Fabricio Martinho Engeroff <fa.engeroff1996@gmail.com>
2022-10-12 09:37:32 -0300

    Pagination/Page

--- Backend/src/main/java/com/DevFabricio/projetoCrud/services/CategoryService.java
+++ Backend/src/main/java/com/DevFabricio/projetoCrud/services/CategoryService.java
@@ -1,13 +1,15 @@
 package com.DevFabricio.projetoCrud.services;
 
 
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
+
+import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,8 +19,6 @@ import com.DevFabricio.projetoCrud.repositories.CategoryRepository;
 import com.DevFabricio.projetoCrud.services.esceptions.DatabaseException;
 import com.DevFabricio.projetoCrud.services.esceptions.ResourceNotFoundException;
 
-import javax.persistence.EntityNotFoundException;
-
 
 @Service
 public class CategoryService {
@@ -27,11 +27,11 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<CategoryDTO> findAll(){
-		List<Category>list =repository.findAll();
-		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+	public Page<CategoryDTO> findAllPaged(PageRequest pageRequest) {
+		 Page<Category> list =repository.findAll(pageRequest);
+			return list.map(x -> new CategoryDTO(x));
 	}
-
+	
 	@Transactional(readOnly = true)
 	public CategoryDTO findById(Long id) {
 		Optional<Category> obj =repository.findById(id);
@@ -72,4 +72,7 @@ public class CategoryService {
 			}
 			
 		}
-}
+
+		
+
+		}
